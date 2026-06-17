c0fd0b8a740384e591ed8bb62ebdf867716b8276
Maria Khan <memaria210@gmail.com>
2026-06-17 15:20:14 +0530

    Add delayed_delivery config switch

--- lib/advanced_sneakers_activejob/configuration.rb
+++ lib/advanced_sneakers_activejob/configuration.rb
@@ -19,6 +19,10 @@ module AdvancedSneakersActiveJob
     config_accessor(:retry_delay_proc) { ->(count) { AdvancedSneakersActiveJob::EXPONENTIAL_BACKOFF[count] } } # seconds
     config_accessor(:log_level) { :info } # debug logs are too noizy because of Bunny
 
+    # :legacy or :leveled. Accepts a symbol or a callable returning one.
+    # Adapter dispatches per-publish via this value.
+    config_accessor(:delayed_delivery) { :legacy }
+
     config_accessor(:publish_connection)
 
     def republish_connection=(_)
